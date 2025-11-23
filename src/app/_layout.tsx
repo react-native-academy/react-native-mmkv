543c108dc42f56fa08d6cb0b4430cb0f6df50a3a
David Grammatico <david.lugdunum@gmail.com>
2025-11-23 10:51:12 +0100

    add button +

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,25 @@
-import { Stack } from "expo-router";
+import { Ionicons } from '@expo/vector-icons';
+import { Stack } from 'expo-router';
+import { Pressable } from 'react-native';
 
 export default function RootLayout() {
-  return <Stack />;
+	return (
+		<Stack>
+			<Stack.Screen
+				name="index"
+				options={{
+					headerTitle: 'Liste de courses',
+					headerRight: () => (
+						<Pressable onPress={() => {}}>
+							<Ionicons
+								name="add"
+								size={36}
+								color="#1e90ff"
+							/>
+						</Pressable>
+					),
+				}}
+			/>
+		</Stack>
+	);
 }
